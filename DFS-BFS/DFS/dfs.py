8e55c6cfebd2829d67fcbd721d87c73a77d6d9da
gengmingjin <gengmingjin@bupt.edu.cn>
2019-10-16 09:58:29 +0800

    update

--- DFS-BFS/DFS/dfs.py
+++ DFS-BFS/DFS/dfs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Description: The Deep-First-Search Method for a binary tree(Recurrent).
+Description: The Deep-First-Search Method for a binary tree.
 
 Author: gengmingjin(gengmingjin@bupt.edu.cn)
 Date: 2019/10/15 17:55:22
@@ -13,17 +13,44 @@ from tqdm import trange
 
 from binarytree import SearchBinaryTree
 
-def dfs(root, res):
-    """ Deep-First-Search Method for a tree
+def dfs_recurrent(root, res):
+    """
+    Deep-First-Search Method for a tree(Recurrent)
     Args:
-        root: The root node of a tree. """
+        root: The root node of a tree.
+        res: a blank list to save the visited results.
+    """
     if root == None:
         return res
     res.append(root.value)
     if root.lchild:
-        dfs(root.lchild, res)
+        dfs_recurrent(root.lchild, res)
     if root.rchild:
-        dfs(root.rchild, res)
+        dfs_recurrent(root.rchild, res)
+
+def dfs_nonrecurrent(root):
+    """
+    Deep-First-Search Method for a tree(Non-Recurrent)
+    Args:
+        root: The root node of a tree.
+    """
+    res = []
+    stack = []
+    if root == None:
+        return res
+    stack.append(root)
+
+    while(len(stack) != 0):
+        tmp = stack[-1]
+        res.append(tmp.value)
+        stack.remove(tmp)
+
+        if tmp.rchild:
+            stack.append(tmp.rchild)
+        if tmp.lchild:
+            stack.append(tmp.lchild)
+
+    return res
 
 if __name__ == "__main__":
     # Generate a binary search tree to test the bfs function
@@ -32,6 +59,7 @@ if __name__ == "__main__":
     for i in trange(len(inlst)):
         stree.root = stree.insert(stree.root, inlst[i])
     res = []
-    dfs(stree.root, res)
+    dfs_recurrent(stree.root, res)
     print(res)
+    print(dfs_nonrecurrent(stree.root))
 
